cfdc0b601c68d94d4b50e628c8b696e497555d32
Dinesh Balasubramanian <dineshudt17 - at - gmail - dot - com>
2016-04-08 23:59:27 +0530

    Small refactoring

--- share.exs
+++ share.exs
@@ -1,30 +1,18 @@
-defmodule Share do
-  def calculate(a), do: "Aswin mada a profit of " <> abc(String.split(a, ~r{,}))
+defmodule ShareCalculator do
+  def calculate(inputString), do: "Aswin made a profit of " <> _calculate(String.split(inputString, ~r{,}), 0)
 
-  defp abc([], amount), do: Integer.to_string(amount)
+  defp _calculate([], amount), do: Float.to_string(amount, [decimals: 2])
 
-  defp abc([h| t], amount \\ 0) do
-    IO.inspect h
-    amount = find(amount, h)
-    abc(t, amount)
-  end
+  defp _calculate([h|t], amount), do: _calculate(t, parseAndCalculateEachSentence(h, amount))
 
-  def find(amount, h) do
-    a = String.split(h, ~r{ })
-    IO.inspect a
-    IO.inspect Enum.at(a, 1)
-    if Enum.at(a, 1) == "buys" do
-      IO.inspect amount
-      IO.inspect Enum.at(a, 2)
-      IO.inspect Enum.at(a, 7)
-      {val1, _} = Integer.parse Enum.at(a,2)
-      {val2, _} = Integer.parse Enum.at(a,7)
-      amount = amount + (val1 * val2)
-     end
-    if Enum.at(a, 1) == "sells" do
-      amount = amount - (Enum.at(a, 2) * Enum.at(a, 7))
+  defp parseAndCalculateEachSentence(head, amount) do
+    sentence_array = String.split(head, ~r{ })
+    {val1, _} = Float.parse Enum.at(sentence_array,2)
+    {val2, _} = Float.parse Enum.at(sentence_array,7)
+    if Enum.at(sentence_array, 1) == "buys" do
+      amount + (val1 * val2)
+    else
+      amount - (val1 * val2)
     end
-    IO.inspect amount
-    amount
   end
 end
